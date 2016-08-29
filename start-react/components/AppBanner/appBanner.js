93ac928750942fb4b383a57290508209bb083831
wqq277763070 <wqq277763070@foxmail.com>
2016-08-29 15:30:28 +0800

    test

---
+++ start-react/components/AppBanner/appBanner.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var AppBanner = React.createClass({
+  render: function () {
+    return (
+      <div className="banner">
+        banner
+      </div>
+    );
+  }
+});
+
+module.exports = AppBanner;
